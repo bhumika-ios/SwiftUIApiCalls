75c2721825f7d502549ed27e00305d33a9a705ea
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-03 12:22:46 +0530

    Initial Commit

---
+++ SwiftUIApiCalls/SwiftUIApiCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApiCallsApp.swift
+//  SwiftUIApiCalls
+//
+//  Created by Bhumika Patel on 03/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApiCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
